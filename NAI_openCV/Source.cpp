6ec7ad86771f603975da11652e4abecd76ba7691
s10733 <s10733@pjwstk.edu.pl>
2016-01-08 00:26:26 +0100

    Dodanie rysowania trójkątów z pobranej klatki.

--- NAI_openCV/Source.cpp
+++ NAI_openCV/Source.cpp
@@ -8,7 +8,7 @@
 using namespace cv;
 using namespace std;
 void drawLine();
-
+void drawTriangles(Mat&, const vector< vector<Point> >&);
 
 Mat img_rgb,img_gray,canny_output,drawing;
 
@@ -98,14 +98,34 @@ int main(int, char**)
 		destroyWindow("wykryty czerwony");
 		img_rgb  = imread("C:/Users/£ukasz/Desktop/Nowy folder/ima.png");
 		cvtColor(img_rgb,img_gray,CV_RGB2GRAY);
-		imshow("InputImage",img_rgb);
+		imshow("Klatka",img_rgb);
 		drawLine();
 		(waitKey(10)!=27);
 	}   
 
 }
 void drawLine(){
+	vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
 	Canny( img_gray, canny_output, thresh, thresh*2, 3 );
-    imshow("Canny",canny_output);
+    imshow("Kontury",canny_output);
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+
+    drawTriangles(drawing,contours);
+    imshow("Trojkaty",drawing);;
+}
+void drawTriangles(Mat& img, const vector< vector<Point> >& contours){
+    vector<Point> approxTriangle;
+    for(size_t i = 0; i < contours.size(); i++){
+        approxPolyDP(contours[i], approxTriangle, arcLength(Mat(contours[i]), true)*0.05, true);
+        if(approxTriangle.size() == 3){
+            drawContours(img, contours, i, Scalar(0, 255, 255), CV_FILLED); // fill GREEN
+            vector<Point>::iterator vertex;
+            for(vertex = approxTriangle.begin(); vertex != approxTriangle.end(); ++vertex){
+                circle(img, *vertex, 3, Scalar(0, 0, 255), 1);
+            }
+        }
+    }
 }
 
