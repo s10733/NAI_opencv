6314acd88519efe66fba0ddcac9f9e4b1b33738d
s10733 <s10733@pjwstk.edu.pl>
2016-01-27 20:45:51 +0100

    Drobne zmiany w kodzie

--- NAI_openCV/Source.cpp
+++ NAI_openCV/Source.cpp
@@ -7,32 +7,19 @@
 #include <conio.h>
 using namespace cv;
 using namespace std;
-void drawLine();
-void drawTriangles(Mat&, const vector< vector<Point> >&);
 
 Mat frame,frame2,imgTemp,imgLines,img_rgb,img_gray,canny_output,drawing;
-
-int thresh = 100;
-int max_thresh = 255;
-
 
 int main(int, char**)
 {
 	VideoCapture cap(0); 
     if(!cap.isOpened())  
 		return -1;
-	
-	int iLowH = 170;
-	int iHighH = 179;
 
-	int iLowS = 150; 
-	int iHighS = 255;
-
-	int iLowV = 60;
-	int iHighV = 255;
-	
 	int iLastX = -1; 
 	int iLastY = -1;
+	
+	 
 	cap.read(imgTemp);
 	imgLines = Mat::zeros(imgTemp.size(),CV_8UC3);
 	while (waitKey(30) !=27){
@@ -40,7 +27,7 @@ int main(int, char**)
 		cvtColor(frame,frame2,COLOR_BGR2HSV);
 		//wydzielenie czerwonego obrazu;
 		Mat imgTres;
-		inRange(frame2,Scalar(iLowH,iLowS,iLowV),Scalar(iHighH,iHighS,iHighV),imgTres);
+		inRange(frame2,Scalar(0,109,176),Scalar(10,144,249),imgTres);
 		
 		//wyliczenie momentów progowych
 		Moments oMoments = moments(imgTres);
@@ -59,7 +46,7 @@ int main(int, char**)
 		   if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
 		   {
 			//rysowanie od pocz¹tku do koñca lini.
-			line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+			line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,250,255), 2);
 		   }
    
 		   iLastX = posX;
@@ -89,35 +76,31 @@ int main(int, char**)
 		destroyWindow("linia");
 		destroyWindow("wykryty czerwony");
 		img_rgb  = imread("C:/Users/£ukasz/Desktop/Nowy folder/ima.png");
-		cvtColor(img_rgb,img_gray,CV_RGB2GRAY);
 		imshow("Klatka",img_rgb);
-		drawLine();
+		
+		cvtColor(img_rgb,img_gray,CV_RGB2GRAY);
+		Canny( img_gray, canny_output, 100, 200, 3 );
+		imshow("Kontury",canny_output);
+
+		vector<vector<Point> > contours;
+		findContours( canny_output, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+
+		vector<Point> approxTriangle;
+		for(size_t i = 0; i < contours.size(); i++){
+        approxPolyDP(contours[i], approxTriangle, arcLength(Mat(contours[i]), true)*0.05, true);
+        if(approxTriangle.size() ==3){
+            drawContours(drawing, contours, i, Scalar(0, 255, 255), CV_FILLED); 
+            vector<Point>::iterator vertex;
+            for(vertex = approxTriangle.begin(); vertex != approxTriangle.end(); ++vertex){
+                circle(drawing, *vertex, 3, Scalar(0, 0, 255), 1);
+            }
+        }
+	 }
+
+    imshow("Trojkaty",drawing);;
 		(waitKey(10)!=27);
 	}   
 
-}
-void drawLine(){
-	vector<vector<Point> > contours;
-   
-	Canny( img_gray, canny_output, thresh, thresh*2, 3 );
-    imshow("Kontury",canny_output);
-    findContours( canny_output, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-
-    drawTriangles(drawing,contours);
-    imshow("Trojkaty",drawing);;
-}
-void drawTriangles(Mat& img, const vector< vector<Point> >& contours){
-    vector<Point> approxTriangle;
-    for(size_t i = 0; i < contours.size(); i++){
-        approxPolyDP(contours[i], approxTriangle, arcLength(Mat(contours[i]), true)*0.05, true);
-        if(approxTriangle.size() == 3){
-            drawContours(img, contours, i, Scalar(0, 255, 255), CV_FILLED); // fill GREEN
-            vector<Point>::iterator vertex;
-            for(vertex = approxTriangle.begin(); vertex != approxTriangle.end(); ++vertex){
-                circle(img, *vertex, 3, Scalar(0, 0, 255), 1);
-            }
-        }
-    }
 }
 
